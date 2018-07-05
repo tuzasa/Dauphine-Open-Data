4d18aefee900b15cd5f7aa6a5b3a98b4c764f6bb
tuzasa <tuzamangarasandra@gmail.com>
2018-07-05 10:44:55 +0200

    initialisation du projet avec mvn

---
+++ src/main/java/io/github/sandra/App.java
@@ -0,0 +1,17 @@
+package io.github.sandra;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		new App().proceed();
+	}
+
+	public void proceed() {
+		LOGGER.info("Hello World!");
+	}
+}
